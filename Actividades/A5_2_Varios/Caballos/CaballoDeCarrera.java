93946099f2534c08c8851c3ac1c5ef1e6b791739
AlfredoPalacios <42215205+AlfredoPalacios@users.noreply.github.com>
2019-03-26 08:19:27 -0600

    A Caballo imprime fecha bien

--- Actividades/A5_2_Varios/Caballos/CaballoDeCarrera.java
+++ Actividades/A5_2_Varios/Caballos/CaballoDeCarrera.java
@@ -2,6 +2,7 @@ package Actividades.A5_2_Varios.Caballos;
 
 import java.util.Date;
 import java.util.GregorianCalendar;
+import java.text.SimpleDateFormat;
 import java.util.*;
 
 /**
@@ -10,13 +11,20 @@ import java.util.*;
 public class CaballoDeCarrera extends Caballo {
     protected int carreras;
     protected int carrerasGanadas;
-    public CaballoDeCarrera(){}
-    public CaballoDeCarrera(String nombre, String color, GregorianCalendar fecha, int carreras, int carrerasGanadas){
+
+    public CaballoDeCarrera() {
+    }
+
+    public CaballoDeCarrera(String nombre, String color, GregorianCalendar fecha, int carreras, int carrerasGanadas) {
         super(nombre, color, fecha);
         this.carreras = carreras;
         this.carrerasGanadas = carrerasGanadas;
     }
-    public void printData(){
-        System.out.println("Se llama: "+nombre+" es de color: "+color+" nacio el: "+fecha.toZoneDateTime().format(DateTimeFormatter.ofPattern("d MMM uuuu"))+" ha participado en: "+carreras+" carreras y ha ganado: "+carrerasGanadas+".");
+
+    public void printData() {
+        SimpleDateFormat d = new SimpleDateFormat("dd-MMM-yyyy");
+        String f = d.format(getFecha().getTime());
+        System.out.println("Se llama: " + nombre + " es de color: " + color + " nacio el: " + f + " ha participado en: "
+                + carreras + " carreras y ha ganado: " + carrerasGanadas + ".");
     }
 }
