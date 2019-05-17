53395a629c4a4f108452ac87282297ec7038f8f6
AlfredoPalacios <42215205+AlfredoPalacios@users.noreply.github.com>
2019-05-17 12:10:15 -0500

    Proyecto finl

--- Proyectos/TextinTheCity/Interface.java
+++ Proyectos/TextinTheCity/Interface.java
@@ -1,4 +1,9 @@
 import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.event.KeyListener;
+import java.awt.event.KeyEvent;
+
+import java.io.*;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -9,32 +14,150 @@ import javax.swing.JTextArea;
 import javax.swing.ListSelectionModel;
 import javax.swing.event.ListSelectionEvent;
 import javax.swing.event.ListSelectionListener;
+import javax.swing.event.TableModelListener;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableColumn;
+import javax.swing.table.TableModel;
 
-public class Interface extends JPanel {
+public class Interface extends JPanel implements KeyListener {
 
     JTextArea commandPrompt;
     JTable listOfNotes;
     JTextArea noteEditor;
     JSplitPane splitPane;
     JScrollPane scrollPane;
+    JScrollPane commandPromptScroll;
+    JScrollPane noteEditorScroll;
+    Note note;
+    DefaultTableModel tableModel;
+    int notesAdded;
+    int commitCounter;
+    protected Commit[] commitList;
+    Commit commit;
+    Push push;
 
     public Interface() {
         super(new BorderLayout());
-        commandPrompt = new JTextArea("prompt:>> ");
-        add(commandPrompt, BorderLayout.SOUTH);
-        listOfNotes =new JTable(new Object[][]{{"Note 1"}, {"Note 2"}, {"Note 3"}}, new Object[]{"NOTES"});
+
+        notesAdded = 0;
+        commitCounter = 0;
+
+        commitList = new Commit[100];
+        note = new Note("", "");
+        commit = new Commit(0, "");
+        push = new Push();
+
+        commandPrompt = new JTextArea(10, 5);
+        commandPrompt.setLineWrap(true);
+        commandPrompt.setWrapStyleWord(true);
+        commandPrompt.setForeground(Color.white);
+        commandPrompt.setBackground(Color.black);
+        commandPromptScroll = new JScrollPane(commandPrompt);
+        add(commandPromptScroll, BorderLayout.SOUTH);
+        commandPrompt.addKeyListener(this);
+
+        tableModel = new DefaultTableModel();
+        tableModel.addColumn("NOTES");
+        listOfNotes =new JTable(tableModel);
         scrollPane = new JScrollPane(listOfNotes);
         noteEditor = new JTextArea("Note editor");
-        splitPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, scrollPane, noteEditor);
+        noteEditor.setLineWrap(true);
+        noteEditor.setWrapStyleWord(true);
+        noteEditorScroll = new JScrollPane(noteEditor);
+        splitPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, scrollPane, noteEditorScroll);
         add(splitPane, BorderLayout.CENTER);
-        ListSelectionModel lsm = listOfNotes.getSelectionModel();
-        lsm.addListSelectionListener(new ListSelectionListener(){
-            @Override
-            public void valueChanged(ListSelectionEvent e) {
-                int selectedColumn = lsm.getMinSelectionIndex()+1;
-                noteEditor.setText("Se cambio. Renglon seleccionado: "+ selectedColumn);
+        
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        if(e.getKeyCode()==KeyEvent.VK_ENTER) {
+            String instruction = commandPrompt.getText();
+            String[] instructionThing;
+            instructionThing = instruction.split("/");
+            if(instructionThing[0].equalsIgnoreCase("addNote")){
+                    note.addNote(instructionThing[1].toString(), instructionThing[2].toString());
+                    //System.out.println(instructionThing[2]);
+                    //System.out.println(note.getNoteList()[notesAdded].titleToString());
+                    System.out.println("Note added");
+                    commandPrompt.selectAll();
+                    commandPrompt.replaceSelection("Note added");
+                    tableModel.addRow(new Object[]{note.getNoteList()[notesAdded].titleToString()});
+                    notesAdded++;
+                    ListSelectionModel lsm = listOfNotes.getSelectionModel();
+                    lsm.addListSelectionListener(new ListSelectionListener(){
+                        @Override
+                        public void valueChanged(ListSelectionEvent e) {
+                            int selectedColumn = lsm.getMinSelectionIndex()+1;
+                            noteEditor.setText("\n"+note.getNoteList()[selectedColumn-1].contentToString());
+                        }
+                    });
+//-------------------------------------------------------------------------------------------------------                    
+            } else if(instruction.equalsIgnoreCase("listNotes")){
+                note.listNotes();
+                for(int i = 0; i<notesAdded; i++){
+                    commandPrompt.setText("Note No. "+(i+1)+
+                    "\nTitle: "+note.getNoteList()[i].titleToString()+"\nContent: "+note.getNoteList()[i].contentToString()+
+                    "\n-------------------------------------------------------------------------------\n"+"Notes listed");
+                }
+                System.out.println("Notes listed");
+//-------------------------------------------------------------------------------------------------------         
+            } else if(instructionThing[0].equalsIgnoreCase("findNote")){
+                int position = note.findNote(instructionThing[1]);
+                    if(position!=-1){
+                        commandPrompt.selectAll();
+                        commandPrompt.replaceSelection("Note you're looking for is: \n"+
+                        "Title: "+note.getNoteList()[position].getTitle()+"\nContent: "+note.getNoteList()[position].getContent());
+                    } else {
+                        commandPrompt.selectAll();
+                        commandPrompt.replaceSelection("Note not found with a matching title :(");
+                    }
+//-------------------------------------------------------------------------------------------------------         
+            } else if(instructionThing[0].equalsIgnoreCase("commit")){
+                String commitMsg;
+                commitMsg = instructionThing[1];
+                System.out.println(commitMsg);
+                Commit<Integer, String> commit1 = new Commit<>(commitCounter, commitMsg);
+                commitList[commitCounter] = commit1;
+                commitCounter++;
+                
+                    commit.saveTemporal(note, notesAdded);
+                
+                commandPrompt.selectAll();
+                commandPrompt.replaceSelection("Commit done");
+                System.out.println("Commit done");
+//-------------------------------------------------------------------------------------------------------         
+            } else if(instructionThing[0].equalsIgnoreCase("push")){
+                
+                    push.push(note, notesAdded);
+                
+                commandPrompt.selectAll();
+                commandPrompt.replaceSelection("Push done");
+                System.out.println("Push done");
+//-------------------------------------------------------------------------------------------------------         
+            } else if(instructionThing[0].equalsIgnoreCase("saveChanges")){
+                
+//-------------------------------------------------------------------------------------------------------         
+            } else if(instructionThing[0].equalsIgnoreCase("log")){
+                for(int i = 0; i<commitCounter; i++){
+                    commandPrompt.setText("Log "+(i+1)+"\nID: "+commitList[i].commitIdToString()+"\nMessage: "+commitList[i].commitMsgToString()+
+                    "\n------------------------------------------------------------------------\n");
+                }
             }
-        }); 
+        } else if(e.getKeyCode()==KeyEvent.VK_F1){
+            commandPrompt.selectAll();
+            commandPrompt.replaceSelection("");
+        }
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+        
     }
 
     /*
--- Proyectos/TextinTheCity/Main.java
+++ Proyectos/TextinTheCity/Main.java
@@ -1,14 +1,86 @@
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
+
 import javax.swing.JFrame;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Comparator;
+
 /**
  * Main
  */
 public class Main {
-    public static void main(String[] args) {
+    protected static boolean windowClosed;
+    public Main(){
+        windowClosed = false;
+    }
+    public static void main(String[] args) throws IOException {
         JFrame frame = new JFrame("Final project");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
         frame.setContentPane(new Interface());
         frame.setBounds(650, 250, 500, 500);
         frame.getContentPane().repaint();
+        if(windowClosed){
+            Path index = Paths.get("\\Users\\alfre\\Desktop\\Progra orientada a objetos\\Proyectos\\TextinTheCity\\Commit");
+            if(!Files.exists(index)){
+                index = Files.createDirectories(index);
+            } else {
+                Files.walk(index)
+                     .sorted(Comparator.reverseOrder())
+                     .forEach(t -> {
+                         try {
+                             Files.delete(t);
+                         } catch (IOException e) {
+
+                         }
+                     });
+                if(!Files.exists(index)) {
+                    index = Files.createDirectories(index);
+                }
+            }
+        }
+        WindowListener wl = new WindowListener(){
+        
+            @Override
+            public void windowOpened(WindowEvent e) {
+                
+            }
+        
+            @Override
+            public void windowIconified(WindowEvent e) {
+                
+            }
+        
+            @Override
+            public void windowDeiconified(WindowEvent e) {
+                
+            }
+        
+            @Override
+            public void windowDeactivated(WindowEvent e) {
+                
+            }
+        
+            @Override
+            public void windowClosing(WindowEvent e) {
+                
+            }
+        
+            @Override
+            public void windowClosed(WindowEvent e) {
+                windowClosed = true;
+            }
+        
+            @Override
+            public void windowActivated(WindowEvent e) {
+                
+            }
+        };
+        frame.addWindowListener(wl);
     }
 }
